2041c58f95d575df513957620c0d0b504aec070e
cagataycingoz22 <ccingoz22@ku.edu.tr>
2024-12-09 20:18:00 +0300

    refinement

--- src/domain/Main.java
+++ src/domain/Main.java
@@ -1,3 +1,4 @@
+package domain;
 
 import ui.windows.MainMenu;
 
--- src/ui/windows/MainMenu.java
+++ src/ui/windows/MainMenu.java
@@ -1,15 +1,17 @@
 
 package ui.windows;
-import ui.windows.WindowManager;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class MainPanel extends JPanel implements ActionListener{
-    JButton startButton;
-    JButton helpButton;
-    MainPanel() {
+public class MainMenu extends JFrame implements ActionListener{
+	
+    private JButton startButton;
+    private JButton helpButton;
+    private static MainMenu instance;
+
+    private MainMenu() {
         // Add the background image
         ImageIcon Icon = new ImageIcon("src/assets/LOGO.png");
         JLabel backgroundLabel = new JLabel(Icon);
@@ -26,13 +28,14 @@ public class MainPanel extends JPanel implements ActionListener{
         helpButton.setFont(new Font("Gongster",Font.BOLD,25));
         startButton.setBackground(new Color(101, 67, 33));
         helpButton.setBackground(new Color(101, 67, 33));
-        startButton.setForeground(Color.WHITE);
-        helpButton.setForeground(Color.WHITE);
+        startButton.setForeground(Color.BLACK);
+        helpButton.setForeground(Color.black);
         buttonPanel.add(startButton);
         buttonPanel.add(helpButton);
-        buttonPanel.setBackground(Color.black);
+        buttonPanel.setBackground(Color.BLACK);
         buttonPanel.setOpaque(true);
         // Create the main frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000, 800); // Adjust size as needed
         this.setLayout(new BorderLayout());
         this.add(backgroundLabel, BorderLayout.NORTH); // Add image at the top
@@ -44,16 +47,29 @@ public class MainPanel extends JPanel implements ActionListener{
         this.setVisible(true);
 
     }
+
+    public static MainMenu getInstance() {
+        if (instance == null) {
+            // If no instance exists, create one
+            instance = new MainMenu();
+        }
+        // Return the existing instance
+        return instance;
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==startButton) {
             //open build mode
-            WindowManager.getInstance().openBuildMode();
+            HallDesigner halldesigner = new HallDesigner();
+            halldesigner.setVisible(true);
+            this.setVisible(false);
         }
 
         if(e.getSource()==helpButton){
             //open view help
         }
     }
-
 }
+
