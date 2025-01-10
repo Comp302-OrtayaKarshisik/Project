76a2a64e03eaae244b7f37ac92cffcae46ed4d91
Reşit Mehmet Ayhan <resitmehmetayhan@192.168.1.104>
2025-01-10 20:41:38 +0300

    test for some player functions and few minor change

--- src/domain/agent/PlayerTest.java
+++ src/domain/agent/PlayerTest.java
@@ -1,8 +1,11 @@
 package domain.agent;
 
+import controllers.KeyHandler;
 import domain.Game;
 import domain.collectables.Enchantment;
 import domain.collectables.EnchantmentType;
+import domain.level.GridDesign;
+import domain.util.Coordinate;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -70,73 +73,31 @@ class PlayerTest{
 
     }
 
-    /**
-     * lifeEnchantmentEffectTest(): Test whether life enchantment works properly
-     * Expect: player starts with 3 health
-     *         whenever life enchantment used when player has maximum health
-     *         players life should stay the same (3)
-     *         if life smaller than 3, then health has to increase by one
-     */
     @Test
-    void lifeEnchantmentEffectTest() {
+    void moveTest() {
 
-        //Init player and enchantment
         Player player = new Player();
-        Enchantment life = new Enchantment(EnchantmentType.Life);
+        Game game = Game.getInstance();
+        KeyHandler keyHandler = new KeyHandler();
+        GridDesign[] designs = new GridDesign[4];
+        designs[0] = new GridDesign(16,16,2);
+        designs[1] = new GridDesign(16,16,3);
+        designs[2] = new GridDesign(16,16,3);
+        designs[3] = new GridDesign(16,16,3);
+        game.getDungeon().loadDesigns(designs);
 
-        //Be sure that player starts with 3 health
-        assertEquals(3, player.getHealth());
+        game.setKeyHandler(keyHandler);
 
-        //If health is 3 then player takes extra life
-        player.useEnchantment(life);
-        assertEquals(3, player.getHealth());
+        //Assert player starts at the correct direction
+        assertEquals(new Coordinate(0,0),player.getLocation());
 
-        //Reduce the health of player
-        //Be sure that player indeed has 2 health
-        player.reduceHealth();
-        assertEquals(2, player.getHealth());
+        keyHandler.goUp = true;
+        player.move();
+        assertEquals(new Coordinate(0,1),player.getLocation());
 
-        //Use the life enchantment on player
-        //Be sure that life is indeed increased by 1
-        player.useEnchantment(life);
-        assertEquals(3, player.getHealth());
+
 
     }
 
-    /**
-     * cloakEffectTest(): Test whether clock enchantment works properly
-     * Expect: player takes damage when not invisible
-     *         after using cloak, player should gain invisibility
-     *         and should take no damage
-     */
-    @Test
-    void cloakEffectTest() {
 
-        //Init player and enchantment
-        Player player = new Player();
-        Enchantment cloak = new Enchantment(EnchantmentType.Cloak);
-
-        //Be sure that player is invisible
-        assertFalse(player.isInvisible());
-
-        //Try to reduce the health of the player
-        //Be sure that when not invisible player takes damage
-        //And it's health is 2 now
-        player.reduceHealth();
-        assertEquals(2, player.getHealth());
-
-        //Add enchantment to the bag, collect works from the prev test
-        player.collectEnchantment(cloak);
-
-        //Use enchantment on player
-        //Now has to be
-        player.useEnchantment(cloak);
-        assertTrue(player.isInvisible());
-
-        //try to reduce the health
-        //it shouldn't decrease since player is invisible
-        player.reduceHealth();
-        assertEquals(2, player.getHealth());
-
-    }
 }
